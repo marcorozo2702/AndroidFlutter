a86b7758ea7fb5310484221b2c7240a0337977f8
Marco <marco.rozo@aluno.sc.senac.br>
2019-09-17 21:58:34 -0300

    Projeto contatos (Exemplo Professor)

---
+++ flutter_sqflite-master/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_db_local/ui/contact_page.dart';
+import 'package:flutter_db_local/ui/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
